ce91bddd690c32fe865eeeca9ec98ad193e083cd
TwinkleBlisss <98691634+TwinkleBlisss@users.noreply.github.com>
2022-04-29 01:49:45 +0300

    Update stack.h

--- stack.h
+++ stack.h
@@ -2,14 +2,14 @@
 
 struct Stack {
     double *data;
-    int size;   // Макс. размер массива.
-    int top;    // Фактическое число элементов в массиве.
+    int size;   // РњР°РєСЃ. СЂР°Р·РјРµСЂ РјР°СЃСЃРёРІР°.
+    int top;    // Р¤Р°РєС‚РёС‡РµСЃРєРѕРµ С‡РёСЃР»Рѕ СЌР»РµРјРµРЅС‚РѕРІ РІ РјР°СЃСЃРёРІРµ.
 };
 
 Stack* createStack();
 void deleteStack(Stack** stack);
 void resize(Stack* stack);
 void push(Stack* stack, double value);
-double pop(Stack* stack);
+void pop(Stack* stack);
 double head(const Stack* stack);
 void cut(Stack* stack);
